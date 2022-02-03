43c77959c2b8aaa58758d5d0bce5b73269ac011f
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-02-03 22:41:53 +0100

    Corregido error al mostrar libros
    Si el resultado de una búsqueda son más libros que casillas, solo se mostrarán libros hasta llenar las casillas (de momento, solo 1 página). Si son menos, se mostrarán y se eliminan las casillas sobrantes (de momento sólo se borran la primera vez, tengo que ver cómo borrar una vez se ha realizado una búsqueda)

--- JS/index.js
+++ JS/index.js
@@ -7,6 +7,9 @@ function iniciar() {
     var precios = document.getElementsByClassName("precio");
     var ofertas = document.getElementsByClassName("oferta");
     var botonesCompra = document.getElementsByClassName("comprar");
+
+    var limite;
+     
     function getJSON(libro) {
         var xhr = new XMLHttpRequest(); //Se crea el objeto
         xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + libro, true); //Abrir una petición
@@ -46,32 +49,51 @@ function iniciar() {
         //        index--;
         //     }
         // }
-        var limite;
-        console.log(botonesCompra);
-        if(libros.length<10){
+        
+        if(libros.length<10){//Para no sobrepasar el límite de 9 libros por página
             limite=libros.length;
         }else{
-            limite=libros.length-portadas.length;
+            limite=portadas.length;
         }
-        for (let index = 0; index < limite; index++) {
 
+        // if(libros.length < portadas.length){
+        //     diferencia= portadas.length-libros.length;
+        //     console.log(diferencia);
+        //     for (let index = portadas.length-diferencia; index < portadas.length; index++) {
+        //         portadas[index].parentElement.parentElement.setAttribute("style", "display:block;");
+                
+        //     }
+        // }
+        
+        for (let index = 0; index < limite; index++) {
+            //Se añade la portada, la sinopsis y el título de cada libro
             portadas[index].setAttribute("src", libros[index].volumeInfo.imageLinks.thumbnail);
             descripcion[index].innerText = libros[index].volumeInfo.description;
             titulos[index].innerText = libros[index].volumeInfo.title;
 
             if (libros[index].saleInfo.saleability == "FOR_SALE") {
+                /*
+                Si el libro está a la venta, se muestra el precio y los botones de compra
+                */
                 precios[index].innerText = libros[index].saleInfo.listPrice.amount + "€";
                 botonesCompra[index].setAttribute("style", "display:block;");
                 descripcion[index].parentElement.parentElement.setAttribute("style", "opacity:1;");
                 
                 if(libros[index].saleInfo.retailPrice.amount <libros[index].saleInfo.listPrice.amount){
+                    /*
+                    Si el libro tiene oferta, se tachará el precio sin oferta y se mostrará a su lado el nuevo precio
+                    */
                     precios[index].classList="precio text-decoration-line-through";
                     ofertas[index].innerText=libros[index].saleInfo.retailPrice.amount+" €";
-                }else{
+                }else{//Si no tiene oferta, se deja todo como estaba para que en futuras búsquedas no de información errónea
                     precios[index].classList="precio";
                     ofertas[index].innerText="";
                 }
             } else {
+                /*Si no está a la venta el libro aparecerá con baja opacidad,
+                 se eliminan los botones de compra y en lugar del precio 
+                 se muestra que el producto no está disponible
+                */
                 precios[index].classList="precio";
                     ofertas[index].innerText="";
                 precios[index].innerText = "Producto no disponible";
@@ -81,10 +103,13 @@ function iniciar() {
 
         }
         for (let index = 0; index < descripcion.length; index++) {
+            /*
+                En el caso en el que haya menos libros que tarjetas, se ocultarán las tarjetas restantes
+            */ 
             if (descripcion[index].innerText == "prueba") {
-                console.log(descripcion[index].parentElement.parentElement.parentElement);
-                descripcion[index].parentElement.parentElement.parentElement.styles="display:none";
-                index--;
+                descripcion[index].parentElement.parentElement.parentElement.setAttribute("style", "display:none;");
+            }else{
+                descripcion[index].parentElement.parentElement.parentElement.setAttribute("style", "display:block;");
             }
         }
     }
