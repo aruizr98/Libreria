120367406770e58d3191c63ac53db8a3c9c1392d
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-02-08 10:01:24 +0100

    JS del carrito
    Hace que aparezcan los libros que se han almacenado en el localStorage, así como los botones de comprar y vaciar carrito.

---
+++ JS/carrito.js
@@ -0,0 +1,50 @@
+window.addEventListener("load", iniciar);
+function iniciar() {
+    var cantidad = document.getElementById("cantidad");
+    var datosCesta = JSON.parse(localStorage.getItem("cesta"));
+    var productosCesta = document.getElementById("productosCesta");
+    var vaciar=document.getElementById("vaciar");
+
+    if (datosCesta == null) { // Si la cesta está vacía
+        var div = document.createElement("div");
+        div.append("La cesta está vacía");
+        div.setAttribute("style", "border:1px solid black; font-size:40px; text-align:center");
+
+        productosCesta.append(div);
+        //productosCesta.innerText="Cesta vacía";
+    } else {
+        var total = 0;
+        for (let index = 0; index < datosCesta.length; index++) {
+            if(datosCesta[index].saleInfo.saleability == "FOR_SALE"){
+                let li = document.createElement("li");
+            li.classList = "list-group-item d-flex justify-content-between lh-sm";
+            let div = document.createElement("div");
+            let h6 = document.createElement("h6");
+            h6.innerText = datosCesta[index].volumeInfo.title;
+            h6.classList = "class='my-0'";
+            div.append(h6);
+            li.append(div);
+            let span = document.createElement("span");
+            span.classList = "text-muted";
+            span.innerText = datosCesta[index].saleInfo.retailPrice.amount + " €";
+            li.append(span);
+            productosCesta.append(li);
+            total += datosCesta[index].saleInfo.retailPrice.amount;
+            }
+            
+        }
+        let li2 = document.createElement("li");
+        li2.classList = "list-group-item d-flex justify-content-between";
+        let span = document.createElement("span");
+        span.innerText = "Total: ";
+        li2.append(span);
+        let strong = document.createElement("strong");
+        strong.innerText = total + " €";
+        li2.append(strong);
+        productosCesta.append(li2);
+    }
+    vaciar.addEventListener("click", function(){
+        localStorage.removeItem("cesta");
+        location.reload();
+    })
+}
