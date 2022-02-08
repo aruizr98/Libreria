eb929443295d2ed8e3087b9a7ad1faca39b65eec
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-02-08 21:43:16 +0100

    Mejorada la cesta
    Ahora aparece el autor debajo del título en la cesta.

--- JS/carrito.js
+++ JS/carrito.js
@@ -3,7 +3,7 @@ function iniciar() {
     var cantidad = document.getElementById("cantidad");
     var datosCesta = JSON.parse(localStorage.getItem("cesta"));
     var productosCesta = document.getElementById("productosCesta");
-    var vaciar=document.getElementById("vaciar");
+    var vaciar = document.getElementById("vaciar");
 
     if (datosCesta == null) { // Si la cesta está vacía
         var div = document.createElement("div");
@@ -15,23 +15,26 @@ function iniciar() {
     } else {
         var total = 0;
         for (let index = 0; index < datosCesta.length; index++) {
-            if(datosCesta[index].saleInfo.saleability == "FOR_SALE"){
+            if (datosCesta[index].saleInfo.saleability == "FOR_SALE") {
                 let li = document.createElement("li");
-            li.classList = "list-group-item d-flex justify-content-between lh-sm";
-            let div = document.createElement("div");
-            let h6 = document.createElement("h6");
-            h6.innerText = datosCesta[index].volumeInfo.title;
-            h6.classList = "class='my-0'";
-            div.append(h6);
-            li.append(div);
-            let span = document.createElement("span");
-            span.classList = "text-muted";
-            span.innerText = datosCesta[index].saleInfo.retailPrice.amount + " €";
-            li.append(span);
-            productosCesta.append(li);
-            total += datosCesta[index].saleInfo.retailPrice.amount;
+                li.classList = "list-group-item d-flex justify-content-between lh-sm";
+                let div = document.createElement("div");
+                let h5 = document.createElement("h5");
+                let h6=document.createElement("h6");
+                h5.innerText = datosCesta[index].volumeInfo.title;
+                h6.innerText=datosCesta[index].volumeInfo.authors[0];
+                h5.classList = "my-0";
+                div.append(h5);
+                div.append(h6);
+                li.append(div);
+                let span = document.createElement("span");
+                span.classList = "text-muted";
+                span.innerText = datosCesta[index].saleInfo.retailPrice.amount + " €";
+                li.append(span);
+                productosCesta.append(li);
+                total += datosCesta[index].saleInfo.retailPrice.amount;
             }
-            
+
         }
         let li2 = document.createElement("li");
         li2.classList = "list-group-item d-flex justify-content-between";
@@ -43,7 +46,7 @@ function iniciar() {
         li2.append(strong);
         productosCesta.append(li2);
     }
-    vaciar.addEventListener("click", function(){
+    vaciar.addEventListener("click", function () {
         localStorage.removeItem("cesta");
         location.reload();
     })
