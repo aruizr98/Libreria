3cb92448758272c09121a48cea5f8e44ac0e0f9f
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-02-09 09:29:54 +0100

    Inicio de sesión mejorado.
    El inicio de sesión está adaptado para leer el JSON de usuarios del localStorage.

--- JS/iniciarSesion.js
+++ JS/iniciarSesion.js
@@ -1,25 +1,44 @@
 window.addEventListener("load", iniciar);
-function iniciar(){
-    var email=document.getElementsByName("email")[0];
-    var password=document.getElementsByName("password")[0];
-    var formulario=document.getElementById("inicioSesion");
-    var mensaje=document.getElementById("mensaje");
 
-    formulario.addEventListener("submit", function(e){
+function iniciar() {
+    var email = document.getElementsByName("email")[0];
+    var password = document.getElementsByName("password")[0];
+    var formulario = document.getElementById("inicioSesion");
+    var mensaje = document.getElementById("mensaje");
+    var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    console.log(usuarios);
+
+    formulario.addEventListener("submit", function (e) {
         e.preventDefault();
-        if(email.value==localStorage.getItem("email") && password.value==localStorage.getItem("password")){
-            sessionStorage.setItem("conectado", "true");
-            mensaje.innerText="";
-            mensaje.classList="";
-            location.href="Index.html";
-           
-            
-        }else{
-            console.log("aa");
-            mensaje.innerText="Error. Email o contraseña incorrectos";
-            mensaje.classList="bg-danger";
+        // if(email.value==localStorage.getItem("email") && password.value==localStorage.getItem("password")){
+        //     sessionStorage.setItem("conectado", "true");
+        //     mensaje.innerText="";
+        //     mensaje.classList="";
+        //     location.href="Index.html";
+
+
+        // }else{
+        //     console.log("aa");
+        //     mensaje.innerText="Error. Email o contraseña incorrectos";
+        //     mensaje.classList="bg-danger";
+        //     sessionStorage.setItem("conectado", "false");
+
+        // }
+        var correcto = false;
+        for (let index = 0; index < usuarios.length; index++) {
+            if (email.value == usuarios[index].email && password.value == usuarios[index].password) {
+                sessionStorage.setItem("conectado", "true");
+                mensaje.innerText = "";
+                mensaje.classList = "";
+                correcto = true;
+                localStorage.setItem("nombreUsuario", usuarios[index].nombre);
+                location.href = "Index.html";
+            }
+        }
+        if (!correcto) {
+            mensaje.innerText = "Error. Email o contraseña incorrectos";
+            mensaje.classList = "bg-danger";
             sessionStorage.setItem("conectado", "false");
-        
         }
     });
 
