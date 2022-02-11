5ec163c07be40745dfcd4e54d84ca4c2ac4cc033
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-02-11 16:52:00 +0100

    Arreglado para que no aparezcan duplicados
    Ahora, si un usuario compra un libro 2 veces, aparece solo una vez en su biblioteca.

--- JS/infoLibro.js
+++ JS/infoLibro.js
@@ -13,6 +13,7 @@ function iniciar() {
     var numeroPaginas = document.getElementById("numeroPaginas");
     var formatos = document.getElementById("formatos");
     var libros = JSON.parse(sessionStorage.getItem("resultado"));
+    var botones = document.getElementsByClassName("boton");
     console.log(libroInfo);
     console.log(libros);
     var encontrado = false;
@@ -26,18 +27,18 @@ function iniciar() {
             // if(libros[index].volumeInfo.industryIdentifiers.length <1){
             // isbn.innerHTML="<strong>ISBN: </strong>"+libros[index].volumeInfo.industryIdentifiers[1].identifier;
             // }
-            isbn.innerHTML+="<strong>Identificador: </strong><br/>";
-            isbn.innerHTML+="<ul>";
-                for (let j = 0; j < libros[index].volumeInfo.industryIdentifiers.length; j++) {
-                    console.log(libros[index].volumeInfo.industryIdentifiers[0])
-                    
-                    isbn.innerHTML += "<li><strong>" + libros[index].volumeInfo.industryIdentifiers[j].type + ": </strong>" + libros[index].volumeInfo.industryIdentifiers[j].identifier+"</li>";
+            isbn.innerHTML += "<strong>Identificador: </strong><br/>";
+            isbn.innerHTML += "<ul>";
+            for (let j = 0; j < libros[index].volumeInfo.industryIdentifiers.length; j++) {
+                console.log(libros[index].volumeInfo.industryIdentifiers[0])
 
-                }
-                isbn.innerHTML+="</ul>";
-                if(libros[index].volumeInfo.pageCount !=undefined){
-            numeroPaginas.innerHTML = "<strong>Número de páginas</strong>: " + libros[index].volumeInfo.pageCount;
-                }
+                isbn.innerHTML += "<li><strong>" + libros[index].volumeInfo.industryIdentifiers[j].type + ": </strong>" + libros[index].volumeInfo.industryIdentifiers[j].identifier + "</li>";
+
+            }
+            isbn.innerHTML += "</ul>";
+            if (libros[index].volumeInfo.pageCount != undefined) {
+                numeroPaginas.innerHTML = "<strong>Número de páginas</strong>: " + libros[index].volumeInfo.pageCount;
+            }
             if (libros[index].volumeInfo.publisher != undefined) {
                 editorial.innerHTML = "<strong>Editorial: </strong>: " + libros[index].volumeInfo.publisher;
             }
@@ -56,6 +57,11 @@ function iniciar() {
             }
             if (!libros[index].accessInfo.epub.isAvailable && !libros[index].accessInfo.pdf.isAvailable) {
                 formatos.innerHTML += " Lo sentimos. Este libro no está disponible en ningún formato en este momento.";
+
+                console.log(botones);
+                for (let index = 0; index < botones.length; index++) {
+                    botones[index].setAttribute("style", "display:none;");
+                }
             }
 
         }
